97f6621f544365887c3fe4c8013d00be3ba6770e
bas080 <bas080@hotmail.com>
2013-04-09 15:47:44 +0200

    Removed print and not_increative inventory

--- init.lua
+++ init.lua
@@ -25,7 +25,6 @@ for i = 1, 12 do
   local hue = HUES[i]
   local hue2 = HUES2[i]
   local img = mod.."_"..hue..".png"
-  print(hue)
   --farming
   farming:add_plant(mod..":"..hue, {"tulips:"..hue.."_seeds",mod..":"..hue.."_sprout"}, 60, 2)
   tulips_table[i] = mod..":"..hue
@@ -49,7 +48,7 @@ for i = 1, 12 do
     sunlight_propagates = true,
     walkable = false,
     visual_scale = 1.3,
-    groups = { snappy = 3,flammable=2, floored=1, flower=1},
+    groups = { snappy = 3,flammable=2, floored=1, flower=1, not_in_creative_inventory=1},
     sounds = default.node_sound_leaves_defaults(),
     selection_box = {
       type = "fixed",
@@ -110,10 +109,12 @@ end
 plantslib:spawn_on_surfaces({
   spawn_delay = 36000,
   spawn_plants = tulips_table,
-  avoid_radius = 10,
+  avoid_radius = 50,
   spawn_chance = 1500,
   spawn_surfaces = {"default:dirt_with_grass"},
   avoid_nodes = {"group:flower"},
   seed_diff = tulips_seed_diff,
   light_min = 9
 })
+
+print("["..mod.."] Loaded!")
